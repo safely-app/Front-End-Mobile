d65a7cd3f31801e03d62db6eaf84de59e51cd2af
Hoang-Giang VO <hoang-giang.vo@epitech.eu>
2021-05-23 00:03:09 +0200

    Wrapping the application with the redux-persist, adding into the stack the new pages that we created last commits

--- App.tsx
+++ App.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
 import {Provider} from 'react-redux';
-import {Login, Register} from './src/containers';
 import {store} from './src/redux';
-import {createStackNavigator, StackNavigationOptions} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
-import {RootStackParamList} from './src/routes';
+import {StackNavigationOptions} from '@react-navigation/stack';
+import EntryPoint from './EntryPoint';
 
-const RootStack = createStackNavigator<RootStackParamList>();
+// const RootStack = createStackNavigator<RootStackParamList>();
+
+// const Tab = createBottomTabNavigator();
 
 const options: StackNavigationOptions = {
   headerShown: false
 };
 
 const App: React.FC = () => {
+
   return (
     <Provider store={store}>
-      <StatusBar barStyle="dark-content" hidden />
-      <NavigationContainer>
-        <RootStack.Navigator initialRouteName="Login">
-          <RootStack.Screen name="Login" component={Login} options={options} />
-          <RootStack.Screen name="Register" component={Register} options={options} />
-        </RootStack.Navigator>
-      </NavigationContainer>
+      <EntryPoint />
     </Provider>
   );
 };
