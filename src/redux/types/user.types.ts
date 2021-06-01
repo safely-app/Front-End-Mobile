a6e63abf10fb5e35f7498a6f163cfc95a1ce00ca
hgvo9 <62654884+hgvo9@users.noreply.github.com>
2021-06-02 00:40:40 +0200

    Adding an action to getUserInfo

--- src/redux/types/user.types.ts
+++ src/redux/types/user.types.ts
@@ -3,6 +3,14 @@ export const SET_AUTHENTICATED = 'SET_AUTHENTICATED';
 export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED';
 export const USER_CREATED = 'USER_CREATED';
 export const LOADING_USER = 'LOADING_USER';
+export const GET_USER_INFOS = 'GET_USER_INFOS';
+
+export interface UserInterface {
+  _id: string;
+  email: string;
+  token: string;
+  username: string;
+}
 
 export interface UserLoginInterface {
   _id: string;
@@ -17,6 +25,15 @@ export interface UserRegisterInterface {
   token: string;
 }
 
+export interface UserGetInformation {
+  id: string,
+  username: string,
+  email: string,
+  role: string,
+  createdAt: string,
+  updatedAt: string
+}
+
 interface UserLoginAction {
   type: typeof SET_AUTHENTICATED;
   payload: UserLoginInterface;
@@ -27,4 +44,9 @@ interface UserRegisterAction {
   payload: UserRegisterInterface;
 }
 
-export type UserActionTypes = UserLoginAction | UserRegisterAction;
+interface UserGetInfosAction {
+  type: typeof GET_USER_INFOS;
+  payload: UserGetInformation;
+}
+
+export type UserActionTypes = UserLoginAction | UserRegisterAction | UserGetInfosAction;
