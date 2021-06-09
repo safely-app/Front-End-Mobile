50392e62389fd7d5df6bc066473ef1acbfa741a7
VO Hoang-Giang <hoang-giang.vo@epitecheu>
2021-06-09 22:15:37 +0200

    Added palette of colors used, exporting theme object that has global styling for  the app

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,5 +1,72 @@
 import {StyleSheet} from 'react-native';
 
+var colors: {[key: string]: string};
+
+const paletteLight: typeof colors = {
+  white: "#FFFFFF",
+  blue: "#16304A",
+  orange: "EF4F4F",
+  yellow: "#FFF3AA",
+  steelBlue: "#4179B5",
+  red: "#EF4F4F"
+}
+
+const paletteDark: typeof colors = {
+  // To be defined
+}
+
+// In the future when the paletteDark will be defined, a variable to know if
+// the app will be in dark mode or not, and then we can either set the variable palette
+// to dark or light.
+// const palette = isDarkMode ? paletteDark : paletteLight
+const palette = paletteLight;
+
+export const theme = {
+  colors: palette,
+  sizeText: {
+    xs: 8,
+    s: 12,
+    m: 16,
+    l: 24,
+    xl: 40,
+    xxl: 70
+  },
+  typeText: {
+    h1: {
+      fontFamily: "WorkSans-Bold",
+    },
+    body: {
+      fontFamily: "WorkSans-Regular",
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const colorPlaceholder: string = "#16304A";
+
 export const styles = StyleSheet.create({
     input: {
       height: 40,
@@ -27,6 +94,48 @@ export const styles = StyleSheet.create({
     title: {
       margin: 12,
       fontSize: 30,
-      fontWeight: 'bold'
+      fontFamily: 'WorkSans-Bold'
     }
+});
+
+export const stylesLogin = StyleSheet.create({
+  safelyTitle: {
+    // margin: 30,
+    marginBottom: 25,
+    fontSize: 80,
+    fontFamily: 'WorkSans-Bold',
+    color: "#16304A"
+  },
+  textInput: {
+    height: 35,
+    width: '80%',
+    // marginLeft: 30,
+    borderRadius: 7,
+    color: "#16304A",
+    backgroundColor: "#D6E0EC",
+    paddingLeft: 10
+  },
+  loginContent: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    // alignItems: 'center',
+    marginBottom: 70,
+    marginLeft: 30
+  },
+  button: {
+    height: 40,
+    width: '80%',
+    // margin: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    backgroundColor: "#16304A",
+    color: "white"
+  },
+  horizontalLine: {
+    borderBottomColor: "#16304A",
+    borderBottomWidth: 0.8,
+    width: '25%',
+  }
 });
