5eefc5c6bfbc576cc7ada0080d6c02d138e6815b
giang <hoang-giang.vo@epitech.eu>
2021-08-23 10:31:13 +0200

    Removing useless actions/reducers

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,15 +1,10 @@
 import {combineReducers} from 'redux';
 import userReducer from './user.reducers';
-import commonReducer from './common.reducers';
-import { request, failure, resetFetch } from './common.reducers';
-// import * from './user.reducers';
 
 export const rootReducer = combineReducers({
-  user: userReducer,
-  common: commonReducer
+  user: userReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 export * from './user.reducers';
-// export { request, failure, resetFetch, loginUser, registerUser, getUser, logoutUser };
